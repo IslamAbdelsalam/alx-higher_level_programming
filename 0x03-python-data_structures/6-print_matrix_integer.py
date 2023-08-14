b0975ee9c3fde2f1f158849be1186bfbd5ef57e2
Islam <islamabdoulradi@gmail.com>
2023-08-15 01:56:56 +0300

    format string

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,6 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for item in row:
             if item == row[-1]:
-                print(item)
+                print("{}".format(item))
             else:
-                print(item, end=" ")
+                print("{}".format(item), end=" ")
