e8fcf82c48214f9471ee8f10e17b88dd3e9f1455
Islam <islamabdoulradi@gmail.com>
2023-08-15 20:56:29 +0300

    for loop

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 10):
-    for j in range(0, 10):
-        if i + j != 18:
-            print("{}{}, ".format(i, j), end="")
-        else:
-            print("{}{}".format(i, j))
+for number in range(0, 100):
+    if number == 99:
+        print("{}".format(number))
+    else:
+        print("{:02}".format(number), end=", ")
