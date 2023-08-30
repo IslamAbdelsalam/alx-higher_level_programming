2e56b08c79873539c0c90eac179f21f4bbbff9e8
Islam <islamabdoulradi@gmail.com>
2023-08-30 18:56:32 +0300

    excpect

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,15 +3,19 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-    except ValueError:
+    except:
         return False
     return True
 
 # i = 10.5
 # m = 'solo'
+# test_none = None
 
 # res = safe_print_integer(i)
 # print(res)
 
 # res = safe_print_integer(m)
+# print(res)
+
+# res = safe_print_integer(test_none)
 # print(res)
